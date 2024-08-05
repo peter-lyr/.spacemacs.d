365b2a3b1e76af89bb4f818bb3bb7187a6ef249c
peter-lyr <llydrp.ldp@gmail.com>
2024-08-05 20:07:07 +0800

    !run.py

---
+++ run.py
@@ -0,0 +1,23 @@
+import os
+
+cur_dir = os.path.abspath(os.path.curdir)
+# print(cur_dir)
+# os.system("pause")
+
+home = os.getenv("home")
+home = home.replace("\\", "\\\\")
+
+run_reg = os.path.join(cur_dir, 'run.reg')
+
+with open(run_reg, 'wb') as f:
+    reg_content = rf'''
+Windows Registry Editor Version 5.00
+[HKEY_LOCAL_MACHINE\SOFTWARE\GNU]
+[HKEY_LOCAL_MACHINE\SOFTWARE\GNU\Emacs]
+"HOME"="{home}"
+"SPACEMACSDIR"="{home}\\.spacemacs.d"
+    '''.encode("utf-8")
+    reg_content = reg_content.strip()
+    f.write(reg_content)
+
+os.system(run_reg)
