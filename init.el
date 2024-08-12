65da87ecd96aab51a38a156ce81d88ddaef2525c
peter-lyr <llydrp.ldp@gmail.com>
2024-08-12 11:47:27 +0800

    (ibuffer :variables ibuffer-group-buffers-by 'projects)

--- init.el
+++ init.el
@@ -33,6 +33,7 @@ This function should only modify configuration layer settings."
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
    '(
+     (ibuffer :variables ibuffer-group-buffers-by 'projects)
      autohotkey
      ;; ----------------------------------------------------------------
      ;; Example of useful layers you may want to use right away.
